253c63557396992d03acc322d1a631dbb8823615
Arthur Manenti <arthur.manenti.videira@gmail.com>
2024-10-28 21:39:05 -0300

    First commit

--- dist/js/models/negociacao.js
+++ dist/js/models/negociacao.js
@@ -1,27 +1,28 @@
 export class Negociacao {
-    #data;
-    #quantidade;
-    #valor;
+    #data
+    #quantidade
+    #valor
 
-    constructor(data, quantidade, valor) {
-        this.#data = data;
-        this.#quantidade = quantidade;
-        this.#valor = valor;
+    constructor (data, quantidade, valor){
+        this.#data = data
+        this.#quantidade = quantidade
+        this.#valor = valor
     }
 
-    get data() {
-        return this.#data;
+    get data(){
+        return this.#data
     }
 
-    get quantidade() {
-        return this.#quantidade;
+    get quantidade(){
+        return this.#quantidade
     }
 
-    get valor() {
-        return this.#valor;
+    get valor(){
+        return this.#valor
     }
 
-    get volume() {
-        return this.#quantidade * this.#valor;
+    get volume(){
+        return this.#quantidade * this.#valor
     }
+
 }
