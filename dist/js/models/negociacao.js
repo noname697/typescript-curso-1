b5b9e96b7a1b4cc1d81af20a769147845e7a2e62
Arthur Manenti <arthur.manenti.videira@gmail.com>
2024-10-28 21:00:17 -0300

    Aula 1

--- dist/js/models/negociacao.js
+++ dist/js/models/negociacao.js
@@ -1,14 +1,28 @@
 export class Negociacao {
-    constructor(_data, quantidade, valor) {
-        this._data = _data;
-        this.quantidade = quantidade;
-        this.valor = valor;
+    #data
+    #quantidade
+    #valor
+
+    constructor (data, quantidade, valor){
+        this.#data = data
+        this.#quantidade = quantidade
+        this.#valor = valor
     }
-    get volume() {
-        return this.quantidade * this.valor;
+
+    get data(){
+        return this.#data
     }
-    get data() {
-        const data = new Date(this._data.getTime());
-        return data;
+
+    get quantidade(){
+        return this.#quantidade
     }
+
+    get valor(){
+        return this.#valor
+    }
+
+    get volume(){
+        return this.#quantidade * this.#valor
+    }
+
 }
